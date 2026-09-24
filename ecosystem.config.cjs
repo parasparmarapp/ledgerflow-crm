8c93ca9e7cc506dcac3cea094b5c444265da1e5d
AppTheka <paras.parmar1988@gmail.com>
2026-09-24 12:25:25 +0530

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,26 +4,22 @@ module.exports = {
       name: 'crm-backend',
       cwd: './backend',
       script: 'dist/server.js',
+      exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       watch: false,
-      script_mode: 'fork',
       max_memory_restart: '1G',
-      env: {
-      }
     },
+
     {
       name: 'crm-frontend',
       cwd: './web',
       script: 'node_modules/.bin/vite',
       args: 'preview --port 9173 --host 0.0.0.0',
+      exec_mode: 'fork',
       instances: 1,
-      script_mode: 'fork',
       autorestart: true,
       watch: false,
-      env: {
-        NODE_ENV: 'production'
-      }
     }
   ]
 };
